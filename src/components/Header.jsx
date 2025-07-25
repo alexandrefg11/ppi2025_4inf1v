b9e5bff3be75b553c8c8972e57b43caec3f6511c
alexandrefg11 <victoralexandrefigueiredo6@gmail.com>
2025-07-25 18:46:07 +0000

    Atualização 25.07

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
 import styles from "./Header.module.css";
 import { ShoppingBasket } from "lucide-react";
 import { Link } from "react-router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../service/CartContext";
 
 export function Header() {
   const { cart } = useContext(CartContext);
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div className={styles.container}>
       <Link to="/" className={styles.link}>
         <h1>TJA Megastore</h1>
       </Link>
+      {/* Barra de pesquisa */}
+      <input
+        type="text"
+        placeholder="Pesquisar produtos..."
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        className={styles.searchInput}
+      />
       <Link to="/cart" className={styles.link}>
         <div className={styles.cartInfo}>
           <ShoppingBasket size={32} />
