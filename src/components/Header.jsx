c05ed36544449539226eafcf015378f9d515e62e
alexandrefg11 <victoralexandrefigueiredo6@gmail.com>
2025-08-01 10:38:40 +0000

    Atualização 01.08

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,28 @@
 import styles from "./Header.module.css";
 import { ShoppingBasket } from "lucide-react";
 import { Link } from "react-router";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { CartContext } from "../service/CartContext";
 
 export function Header() {
   const { cart } = useContext(CartContext);
-  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div className={styles.container}>
       <Link to="/" className={styles.link}>
         <h1>TJA Megastore</h1>
       </Link>
-      {/* Barra de pesquisa */}
-      <input
-        type="text"
-        placeholder="Pesquisar produtos..."
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-        className={styles.searchInput}
-      />
       <Link to="/cart" className={styles.link}>
         <div className={styles.cartInfo}>
-          <ShoppingBasket size={32} />
+          <div className={styles.cartIcon}>
+            <ShoppingBasket size={32} />
+            {cart.length > 0 && (
+              <span className={styles.cartCount}>
+                {cart.reduce((total, item) => total + item.quantity, 0)}
+              </span>
+            )}
+          </div>
+
           <p>
             Total: ${" "}
             {cart
